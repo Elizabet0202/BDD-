61208530cec8202dfa3a64f39df1d845d853f9bf
Elizaveta <yelizaveta.baklanova@gmail.com>
2025-11-29 16:57:30 +0300

    Test

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,46 +1,72 @@
 package ru.netology.test;
 
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+import ru.netology.page.TransferPage;
 
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import ru.netology.data.DataHelper;
-import ru.netology.page.LoginPage;
 
 public class MoneyTransferTest {
 
-    @Test
-    void shouldTransferMoneyBetweenOwnCards() {
-        // 1. Открываем приложение
-        var loginPage = open("http://localhost:9999", LoginPage.class);
+    private DashboardPage dashboardPage;
 
-        // 2. Логин
+    @BeforeAll
+    static void setUpAll() {
+
+        //System.setProperty("selenide.shutdownHook", "false");
+        //Configuration.holdBrowserOpen = true;
+        //Configuration.reopenBrowserOnFail = false;
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
-
-        // 3. Вводим код подтверждения
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        var dashboardPage = verificationPage.validVerify(verificationCode);
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
 
-        // 4. Подготовка тестовых данных
-        int amount = 1000;
-        var firstCard = DataHelper.getFirstCard();   // источник
-        var secondCard = DataHelper.getSecondCard(); // получатель
+    @Test
+    @DisplayName("should Not Transfer Money Between Own Cards Over Limit")
+    void shouldNotTransferMoneyBetweenOwnCardsOverLimit() {
+        // карта-получатель и карта-донор
+        var firstCard = DataHelper.getFirstCard();    // пополняем её
+        var secondCard = DataHelper.getSecondCard();  // с неё списываем
 
-        int firstBalanceBefore = dashboardPage.getCardBalance(firstCard);
-        int secondBalanceBefore = dashboardPage.getCardBalance(secondCard);
+        // запоминаем начальные балансы
+        int firstCardStartBalance = dashboardPage.getCardBalance(firstCard);
+        int secondCardStartBalance = dashboardPage.getCardBalance(secondCard);
 
-        // 5. Пополняем вторую карту с первой
-        var transferPage = dashboardPage.selectCardToTopUp(secondCard);
-        dashboardPage = transferPage.transfer(amount, firstCard);
+        // выбираем первую карту для пополнения (жмём «Пополнить» на первой)
+        TransferPage transferPage = dashboardPage.selectCardToTopUp(firstCard);
 
-        // 6. Проверяем балансы после перевода
-        int firstBalanceAfter = dashboardPage.getCardBalance(firstCard);
-        int secondBalanceAfter = dashboardPage.getCardBalance(secondCard);
+        // сумма специально БОЛЬШЕ баланса карты-донора
+        int amount = secondCardStartBalance + 1_000;
 
-        assertEquals(firstBalanceBefore - amount, firstBalanceAfter);
-        assertEquals(secondBalanceBefore + amount, secondBalanceAfter);
+        // пытаемся перевести с второй карты на первую
+        dashboardPage = transferPage.transfer(amount, secondCard);
+
+        // читаем балансы после попытки перевода
+        int actualFirstCardBalance = dashboardPage.getCardBalance(firstCard);
+        int actualSecondCardBalance = dashboardPage.getCardBalance(secondCard);
+
+        // ожидаем, что они НЕ изменились
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(
+                        firstCardStartBalance,
+                        actualFirstCardBalance,
+                        "Баланс первой карты не должен меняться при переводе сверх лимита"
+                ),
+                () -> Assertions.assertEquals(
+                        secondCardStartBalance,
+                        actualSecondCardBalance,
+                        "Баланс второй карты не должен меняться при переводе сверх лимита"
+                )
+        );
     }
 }
-
